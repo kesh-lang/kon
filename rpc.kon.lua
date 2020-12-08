d2fdce7e9c056cba534e49fd35f650f601bd897c
Joakim Stai <joakim@users.noreply.github.com>
2020-12-08 18:00:41 +0000

    Update rpc.kon.lua

--- rpc.kon.lua
+++ rpc.kon.lua
@@ -1,11 +1,11 @@
 -- example based on JSON-RPC 2.0
 
--- send
+--- send
 rpc: 1.0
 req: #subtract { minuend: 42, subtrahend: 23 }
 id: 42
 
--- receive
+--- receive
 rpc: 1.0
 res: 19
 id: 42
@@ -13,21 +13,21 @@ id: 42
 
 -- example of multiple calls in one (a database transaction)
 
--- send
+--- send
 rpc: 1.0
 req:
   foo: #insert({ name: 'foo' })
   bar: #insert({ name: 'bar' })
 id: 43
 
--- receive (success)
+--- receive (success)
 rpc: 1.0
 res:
   foo: { nInserted: 1 }
   bar: { nInserted: 1 }
 id: 43
 
--- receive (error)
+--- receive (error)
 rpc: 1.0
 res: ()
 err:
