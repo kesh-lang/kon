904521ad70a393de09364288b112347cb29affb4
Joakim Stai <joakim@users.noreply.github.com>
2020-12-08 19:48:21 +0000

    Update rpc.kon.lua

--- rpc.kon.lua
+++ rpc.kon.lua
@@ -16,21 +16,21 @@ id: 42
 --- send
 rpc: 1.0
 req:
-  foo: #insert({ name: 'foo' })
-  bar: #insert({ name: 'bar' })
+	foo: #insert({ name: 'foo' })
+	bar: #insert({ name: 'bar' })
 id: 43
 
 --- receive (success)
 rpc: 1.0
 res:
-  foo: { nInserted: 1 }
-  bar: { nInserted: 1 }
+	foo: { nInserted: 1 }
+	bar: { nInserted: 1 }
 id: 43
 
 --- receive (error)
 rpc: 1.0
 res: ()
 err:
-  label: 'TransientTransactionError'
-  message: 'Transaction 1 does not exist'
+	label: 'TransientTransactionError'
+	message: 'Transaction 1 does not exist'
 id: 43
