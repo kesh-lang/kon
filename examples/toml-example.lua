f97a79212876af74be9a0202d5de8774b6c558de
Joakim <joakim@users.noreply.github.com>
2021-05-05 14:57:46 +0200

    Update toml-example.lua

--- examples/toml-example.lua
+++ examples/toml-example.lua
@@ -1,21 +1,31 @@
 -- This is a sode document
 
-title: 'Example'
+title: 'Sode Example'
 
 owner:
-	name: 'Tom Preston-Werner'
-	dob: #instant '1979-05-27T07:32:00-08:00'
+    name: 'Tom Preston-Werner'
+    dob: #instant '1979-05-27T07:32:00-08:00'  -- First class dates
 
 database:
-	enabled: true
-	ports: [ 8001, 8001, 8002 ]
-	data: [ ['delta', 'phi'], [3.14] ]
-	temp-targets: { cpu: 79.5, case: 72.0 }
+    server: '192.168.1.1'
+    ports: [ 8001, 8001, 8002 ]
+    connections-max: 5000
+    enabled: true
 
 servers:
-	alpha:
-		ip: #IPv4 '10.0.0.1'
-		role: 'frontend'
-	beta:
-		ip: #IPv4 '10.0.0.2'
-		role: 'backend'
+    -- Braces are not required for multiline records
+    alpha:
+        ip: '10.0.0.1'
+        dc: 'eqdc10'
+    beta:
+        ip: '10.0.0.2'
+        dc: 'eqdc10'
+
+clients:
+    data: [ ["gamma", "delta"], [1, 2] ]
+
+-- Line breaks can be used to separate array items
+hosts: [
+    "alpha"
+    "omega"
+]
