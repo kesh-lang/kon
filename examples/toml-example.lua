4f17d1e0cbe91604d2d4298ec23eae174091fb0d
Joakim <joakim@users.noreply.github.com>
2021-05-18 14:30:06 +0200

    Update toml-example.lua

--- examples/toml-example.lua
+++ examples/toml-example.lua
@@ -4,20 +4,20 @@ title: 'Sode Example'
 
 owner:
     name: 'Tom Preston-Werner'
-    dob: #instant '1979-05-27T07:32:00-08:00'  -- First class dates
+    dob: instant '1979-05-27T07:32:00-08:00'  -- First class dates
 
 database:
-    server: #ipv4 '192.168.1.1'  -- Tagged values
+    server: ipv4 '192.168.1.1'  -- Tagged values
     ports: (8000, 8001, 8002)
     connections-max: 5000
     enabled: true
 
 servers:  -- Brackets are not required for multiline records
     alpha:
-        ip: #ipv4 '10.0.0.1'
+        ip: ipv4 '10.0.0.1'
         dc: 'eqdc10'
     beta:
-        ip: #ipv4 '10.0.0.2'
+        ip: ipv4 '10.0.0.2'
         dc: 'eqdc10'
 
 clients.data: (('gamma', 'delta'), (1, 2))
